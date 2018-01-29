7aaba59578dabfde7a75d5fc6da0261aa3f92410
Hank Tom <hanktom@gmail.com>
2018-01-29 21:50:56 +0800

    危險權限實作

---
+++ app/src/main/java/com/tom/contentapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.tom.contentapp;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static android.Manifest.permission.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CONTACTS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int permission = ActivityCompat.checkSelfPermission(this,
+                READ_CONTACTS);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            //未取得權限，向使用者要求允許權限
+            ActivityCompat.requestPermissions( this,
+                    new String[]{READ_CONTACTS, WRITE_CONTACTS},
+                    REQUEST_CONTACTS );
+        }else{
+            //已有權限，可進行檔案存取
+            readContacts();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CONTACTS:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //取得聯絡人權限，進行存取
+                    readContacts();
+                } else {
+                    //使用者拒絕權限，顯示對話框告知
+                    new AlertDialog.Builder(this)
+                            .setMessage("必須允許聯絡人權限才能顯示資料")
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+                return;
+        }
+    }
+    
+    private void readContacts() {
+
+    }
+}
